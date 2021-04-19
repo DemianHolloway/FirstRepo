b49a10fc2f904f7e0567d7a1bbecd40936b999b1
WonHyeYoung <demian_holloway@naver.com>
2021-04-19 15:08:14 +0900

    브런치b = 6

--- GitStudy2/src/exam/Question2.java
+++ GitStudy2/src/exam/Question2.java
@@ -4,11 +4,13 @@ public class Question2 {
 
 	public static void main(String[] args) {
 		System.out.println(Math.random());
+		
+		System.out.println("RANDOM!!");
+		
+		// branchA, B 모두에 커밋을 2개 추가하고 history 내역을 보낼 것
+		// (단, Question2만 수정해야 함 *이 주석은 branchB 에서 작성되었음)
 
-		System.out.println("커밋1");
-		System.out.println("커밋2");
-		
-		
+		System.out.println("1+5=6");
 	}
 
 }
