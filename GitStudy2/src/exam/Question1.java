5f3fdc0b07e1ca3d9ca8140f99b0e0acf826a100
pc <pc@HOME-WONHYEJIN>
2021-04-19 11:37:47 +0900

    문제 1 풀고 저장-수정

---
+++ GitStudy2/src/exam/Question1.java
@@ -0,0 +1,12 @@
+package exam;
+
+public class Question1 {
+	
+	public static void main(String[] args) {
+		System.out.println(" /)/) ");
+		System.out.println("( . .)");
+		System.out.println("  > <) ");
+		
+	}
+
+}
