8609cf19cda78232a5f3f70f2c18218db78bae18
pc <pc@HOME-WONHYEJIN>
2021-04-19 11:41:52 +0900

    커밋후 두번째!

--- GitStudy2/src/exam/Question1.java
+++ GitStudy2/src/exam/Question1.java
@@ -4,8 +4,8 @@ public class Question1 {
 	
 	public static void main(String[] args) {
 		System.out.println(" /)/) ");
-		System.out.println("( . .)");
-		System.out.println("  > <) ");
+		System.out.println("( x x)");
+		System.out.println("  >*<) ");
 		
 	}
 
