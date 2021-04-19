346c9360f805388e084b164136e0dad52b893d8a
WonHyeYoung <demian_holloway@naver.com>
2021-04-19 13:09:42 +0900

    랜덤추가

---
+++ GitStudy2/src/exam/Question3.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		int apple = 13;
+		
+		System.out.println(apple/10);
+
+		System.out.println("BASKET!");
+		System.out.println("¹Ù±¸´Ï!");
+	}
+}
