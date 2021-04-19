5b449daafdbed221b90834bf529600fefa549162
WonHyeYoung <demian_holloway@naver.com>
2021-04-19 14:38:08 +0900

    브런치A생성 기념

--- GitStudy2/src/exam/Question1.java
+++ GitStudy2/src/exam/Question1.java
@@ -7,6 +7,9 @@ public class Question1 {
 		System.out.println("( x x)");
 		System.out.println("  >*<) ");
 		
+		System.out.println("Have a Good Time");
+		
+		System.out.println("branchA »ý¼º ±â³ä");
 	}
 
 }
